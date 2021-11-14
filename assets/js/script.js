f660e629328bacba27ea4ca960c6177096af7268
Reht <reht.dev@gmail.com>
2021-11-14 15:51:32 -0600

    character variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
 // Assignment code here
 
 function generatePassword() {
-  console.log("button click")
+  console.log("generate click");
 
-  return "Your new secure password!"
+
+//variables
+var length = 0
+var num = ["0123456789"];
+var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lower = ["abcdefghijklmnopqrstuvwxyz"];
+var special = ["!@#$%^&*()=+-"];
+var allChar = num + upper + lower + special;
+
+
+//prompt user for pass criteria
+  //password length 8 < 128
+  //character types
+while (length < 8 || length > 128 || isNaN(length)) {
+    length = parseInt(window.prompt('Choose a passsword length between 8-128'))
+  }
+
+
+
+//validate selections
+//generate password
+//display to page
+
+  return 
 }
 
 // Get references to the #generate element
