27aaca73d3910df971f36d3164aab5cd88b43238
Reht <reht.dev@gmail.com>
2021-11-14 16:44:00 -0600

    characterSet and selection alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,40 @@
 function generatePassword() {
   console.log("generate click");
 
+  var length = 0
+  var isNumeric = false
+  var isUpper = false
+  var isLower = false
+  var isSpecial = false
 
-//variables
-var length = 0
-var num = ["0123456789"];
-var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lower = ["abcdefghijklmnopqrstuvwxyz"];
-var special = ["!@#$%^&*()=+-"];
-var allChar = num + upper + lower + special;
-
-
-//prompt user for pass criteria
+  //prompt user for pass criteria
   //password length 8 < 128
-  //character types
-while (length < 8 || length > 128 || isNaN(length)) {
+  while (length < 8 || length > 128 || isNaN(length)) {
     length = parseInt(window.prompt('Choose a passsword length between 8-128'))
   }
 
 
+  //validate selections
+  while (!(isNumeric || isUpper || isLower || isSpecial)) {
+    isNumeric = window.confirm('Inclusde numbers?')
+    isUpper = window.confirm('Include uppercase?')
+    isLower = window.confirm('Inslude lowercase?')
+    isSpecial = window.confirm('Include special characters?')
+  }
 
-//validate selections
-//generate password
-//display to page
+  //character types
+  var characterSet = ''
+  if (isNumeric) characterset += char('0123456789')
+  if (isUpper) characterset += ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  if (isLower) characterset += ("abcdefghijklmnopqrstuvwxyz")
+  if (isSpecial) characterset += (" !@#$%^&*()=+-")
 
-  return 
+  var allChar = isNumeric + isUpper + isLower + isSpecial;
+
+  //generate password
+  //display to page
+
+  return
 }
 
 // Get references to the #generate element
